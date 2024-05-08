9e20dd758813f521a995ed86f7343190c6f7aff0
Alina Makarenko <alinamakarenko1306@gmail.com>
2024-05-08 13:18:53 -0500

    added js and style

---
+++ index.js
@@ -0,0 +1,44 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1");
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
+let homeScoreBtnThree = document.getElementById("home-score-btn-3");
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1");
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2");
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3");
+
+let homeScoreEl =document.getElementById("home-score");
+let guestScoreEl =document.getElementById("guest-score");
+let homeScore = 0;
+let guestScore = 0;
+
+function increaseHomeScore(scoreIncreaser){
+    homeScore += scoreIncreaser;
+    console.log(arguments[0]);
+    homeScoreEl.textContent = homeScore;
+}
+function increaseGuestScore(scoreIncreaser){
+    guestScore += scoreIncreaser;
+    console.log(arguments[0]);
+    guestScoreEl.textContent = guestScore;
+}
+
+// Event listeners for home score buttons
+homeScoreBtnOne.addEventListener("click", function() {
+    increaseHomeScore(1);
+});
+homeScoreBtnTwo.addEventListener("click", function() {
+    increaseHomeScore(2);
+});
+homeScoreBtnThree.addEventListener("click", function() {
+    increaseHomeScore(3);
+});
+
+// Event listeners for guest score buttons
+guestScoreBtnOne.addEventListener("click", function() {
+    increaseGuestScore(1);
+});
+guestScoreBtnTwo.addEventListener("click", function() {
+    increaseGuestScore(2);
+});
+guestScoreBtnThree.addEventListener("click", function() {
+    increaseGuestScore(3);
+});
